0d7fb83cfe07dfba196ebb5922f3ccdf1d23e62e
PavelMasko <pm.masko@gmail.com>
2022-04-29 22:30:55 +0300

    feat: repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+// repeatTimes устанавливает число повторений str
+// separator это строка, разделяющая повторения str
+// addition это дополнительная строка, которая будет добавлена после каждого повторения str
+// additionRepeatTimes устанавливает число повторений addition
+// additionSeparator это строка, разделяющая повторения addition
+
+function repeater(str, options) {
+    str = String(str);
+    let result = "";
+    let addition = "";
+
+
+    const optionalParams = {
+        repeatTimes: 1,
+        separator: '+',
+        addition: "",
+        additionSeparator: '|',
+        additionRepeatTimes: 1,
+    }
+    for (let key in options) {
+        optionalParams[key] = options[key];
+    }
+    if (optionalParams['addition'] === '') {
+        addition = "";
+    } else if (optionalParams['repeatTimes'] == 1 && optionalParams['additionRepeatTimes'] > 1) {
+        addition = (optionalParams['addition'] + optionalParams['additionSeparator']).repeat(optionalParams['additionRepeatTimes']).slice(0, -optionalParams['additionSeparator'].length);
+    } else if (optionalParams['repeatTimes'] == 1) {
+        addition = optionalParams['addition']
+    } else if (optionalParams['additionRepeatTimes'] == 1) {
+        addition = optionalParams['addition']
+    } else {
+        addition = (optionalParams['addition'] + optionalParams['additionSeparator']).repeat(optionalParams['additionRepeatTimes']).slice(0, -optionalParams['additionSeparator'].length);
+    }
+
+    result = (str + addition + optionalParams['separator']).repeat(optionalParams['repeatTimes']).slice(0, -optionalParams['separator'].length)
+
+    return result;
+
 }
 
 module.exports = {
-  repeater
+    repeater
 };
+
+const objWithSpecificCoercion = {
+    [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+};
+console.log(objWithSpecificCoercion);
