d20a6da0dbc7fe0184b86839eb8df0d295de96c0
PavelMasko <pm.masko@gmail.com>
2022-04-30 17:40:06 +0300

    feat: season/transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+    let result = [];
+
+    if (!(Array.isArray(arr))) {
+        throw new Error('\'arr\' parameter must be an instance of the Array!');
+    } else if (arr.length == 0) {
+        return result;
+    }
+
+    let newArr = [...arr]
+
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i] == '--discard-next') {
+            delete newArr[i + 1]
+        } else if (newArr[i] == '--discard-prev') {
+            if (result[i - 1] == newArr[i - 1]) {
+                delete result[i - 1]
+            }
+            delete newArr[i - 1]
+        } else if (newArr[i] == '--double-next') {
+            if (newArr[i + 1] != undefined) {
+                result.push(newArr[i + 1])
+            }
+        } else if (newArr[i] == '--double-prev') {
+            if (newArr[i - 1] != undefined)
+                result.push(newArr[i - 1])
+        } else if (newArr[i] != undefined) {
+            result.push(newArr[i])
+        }
+    }
+    return result.filter(item => item != undefined)
 }
 
+// // [1, 2, 3, 4, 4, 5]
 module.exports = {
-  transform
+    transform
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+    if (date === undefined) {
+        return "Unable to determine the time of year!";
+    } else if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0) {
+        throw new Error("Invalid date!");
+    }
+
+
+    const season = date.getMonth();
+    if (season == 0 || season == 1 || season == 11) {
+        return "winter";
+    } else if (season == 2 || season == 3 || season == 4) {
+        return "spring";
+    } else if (season == 5 || season == 6 || season == 7) {
+        return "summer";
+    } else {
+        return "autumn";
+    }
 }
 
 module.exports = {
-  getSeason
+    getSeason
 };
